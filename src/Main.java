28e83cc4a8fc95f7c676aefd04c7ee0759b68342
Seu Nome <mikathor@Mika>
2024-09-30 21:58:30 -0300

    test validacion

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.SortedMap;
+public class Main {
+    public static void main(String[] args) {
+        int indice= 0;
+        int soma = 0;
+        int k = 0;
+
+        indice= 13;
+        soma= 0;
+        k= 0 ;
+
+        while (k < indice) {
+            k = k + 1;
+            soma = soma + k;
+        }
+
+        System.out.println(soma);
+    }
+    }
